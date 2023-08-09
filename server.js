60270eee4ddb538bbf9d2d62e81353668ae29284
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-09 12:07:17 -0300

    Estrutura Inicial

---
+++ server.js
@@ -0,0 +1,8 @@
+import app from "./app";
+
+const porta = 3001;
+app.listen(porta, () => {
+    console.log();
+    console.log("Escutando na porta " + porta);
+    console.log(`CTRL + Clique em http://localhost:${porta}`);
+});
