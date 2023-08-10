07ce973536a34cae3b902fe14d9caa44b81bdfe1
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-10 15:31:28 -0300

    Tentativa de correção da migration unique

--- src/database/migrations/20230810173854-mudar-email-aluno-unique.js
+++ src/database/migrations/20230810173854-mudar-email-aluno-unique.js
@@ -2,21 +2,17 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.changeColumn(
+            'tbl_Alunos',
+            'email',
+            {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+        );
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down() {},
 };
